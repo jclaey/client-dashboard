3586344eee7b3e5cc74ffc8c72e218d2bed3d646
Jason Claeys <jdclaeys@outlook.com>
2025-02-19 15:55:13 -0600

    initial middleware setup

---
+++ backend/routes/contacts/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+const router = express.Router()
+import multer from 'multer'
+import { storage } from '../../cloudinary/index.js'
+import asyncHandler from '../../middleware/async.js'
+import { imageFileFilter } from '../../utils/fileFilter.js'
+import { handleValidationErrors } from '../../middleware/validationErrors.js'
+const upload = multer({ 
+    storage,
+    imageFileFilter,
+    limits: { fileSize: 1024 * 1024 * 5 }
+})
+

3453dfa7df0b10eb063ad6220af910cf77ca2b98
Jason Claeys <jdclaeys@outlook.com>
2025-02-21 21:01:50 -0600

    begin implementing user register

---
+++ frontend/src/api.js
@@ -0,0 +1,40 @@
+import axios from "axios"
+import { getAccessToken, setAccessToken } from "./utils/authHelpers.js"
+
+const api = axios.create({
+    baseURL: "http://localhost:5000/api",
+    withCredentials: true,
+})
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config
+        
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true
+
+            try {
+                const refreshResponse = await axios.post(
+                    "http://localhost:5000/api/users/refresh",
+                    {},
+                    { withCredentials: true }
+                )
+
+                const newAccessToken = refreshResponse.data.accessToken
+
+                setAccessToken(newAccessToken)
+
+                originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`
+                return api(originalRequest)
+            } catch (refreshError) {
+                console.error("Token refresh failed:", refreshError)
+                return Promise.reject(refreshError)
+            }
+        }
+
+        return Promise.reject(error)
+    }
+)
+
+export default api
